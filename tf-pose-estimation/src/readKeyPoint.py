3f336267c1215350b8fb411d20fb4ca761be30bc
Abu <abubakar.saad.891@gmail.com>
2018-04-09 23:35:47 -0400

    Finished the script that reads data for everyhuman in the image. Per human per row

--- tf-pose-estimation/src/readKeyPoint.py
+++ tf-pose-estimation/src/readKeyPoint.py
@@ -9,26 +9,38 @@ myData = []
      
 
 def readKeyPoints(bodyIndex, bodyPartx, bodyParty):
-    myFile = open('../../example2.csv', 'w')
-    print("--------")
-    print(bodyIndex, bodyPartx, bodyParty)
+    
+    # print("--------")
+    # print(bodyIndex, bodyPartx, bodyParty)
     myData.append([bodyIndex, bodyPartx, bodyParty])
 
 
 def storeData():
     global myData
+    myFile = open('/Users/abubakarsaad/Documents/MachineLearning/4P76Poser/example2.csv', 'a')
+    # print(myFile)
+    # writer = csv.writer(myFile)
     i = 0
     j = 0
+    
     while(i < 18):
-        print(i, j)
+        # print(i, j)
 
         if(i < len(myData) and myData[j][0] == i):
-            print(myData[i][1], myData[i][2])
+            # print(myData[i][1], myData[i][2])
+            myFile.write(str(myData[i][1]) + ',')           
+            myFile.write(str(myData[i][2]) + ',')
             j+=1
         else:
-            print("Insert new array")
+            # print("Insert new array")
+            myData.insert(i, [i, -1, -1])
+            myFile.write(str(-1) + ',')
+            myFile.write(str(-1) + ',')
+            j+=1
         i+=1
-    print(myData)
+        # print(myData)
+
+
 
 
 
