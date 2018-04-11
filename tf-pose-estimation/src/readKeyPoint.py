cd4af0035a3d9fed34ecf4e4e170c50882f88e73
MattKirchhof <matthewkirchhof@hotmail.com>
2018-04-11 14:29:34 -0400

    Added data normalization

--- tf-pose-estimation/src/readKeyPoint.py
+++ tf-pose-estimation/src/readKeyPoint.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 myData = []
  
@@ -15,9 +16,40 @@ def readKeyPoints(bodyIndex, bodyPartx, bodyParty):
     myData.append([bodyIndex, bodyPartx, bodyParty])
 
 
+def normalizeData():
+    global myData
+
+    minX = 999999
+    minY = 999999
+    maxX = 0
+    maxY = 0
+
+    # get min and max vals
+    for dataPiece in myData:
+        if (dataPiece[1] < minX):
+            minX = dataPiece[1]
+        elif (dataPiece[1] > maxX):
+            maxX = dataPiece[1]
+        if (dataPiece[2] < minY):
+            minY = dataPiece[2]
+        elif (dataPiece[2] > maxY):
+            maxY = dataPiece[2]
+    
+    # normalize all data with respect to min and max values of x and y
+    for dataPiece in myData:
+        # normalize X value
+        dataPiece[1] = (dataPiece[1] - minX) / (maxX - minX)
+        # normalize Y value
+        dataPiece[2] = (dataPiece[2] - minY) / (maxY - minY)
+
+    #done?
+
 def storeData():
     global myData
-    myFile = open('/Users/abubakarsaad/Documents/MachineLearning/4P76Poser/example2.csv', 'a')
+
+    normalizeData()
+
+    myFile = open('../../squattingDataSet.csv', 'a')
     # print(myFile)
     # writer = csv.writer(myFile)
     i = 0
