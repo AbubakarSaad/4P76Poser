fac5c8a105bbc0c9b2587179740e7d6de2e450ff
sulman633 <sulmanqureshi1@gmail.com>
2018-04-11 20:25:42 -0400

    Created a basic tensor network

---
+++ tensor_network.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+import numpy as np
+
+# Sample Dataset
+x_data = np.array([
+[0.,0.], [0.,1.], [1.,0.], [1.,1.]
+])
+y_data = np.array([
+[0.], [1.], [1.], [0.]
+])
+
+# output_layerperparamters
+n_input = 2
+n_hidden = 10
+n_output = 1
+learning_rate = 0.1
+epochs = 100
+display_step = 10
+
+# Placeholders
+inputs = tf.placeholder(tf.float32)
+expected_output = tf.placeholder(tf.float32)
+
+# Weights
+weights_1 = tf.Variable(tf.random_uniform([n_input, n_hidden], -1.0, 1.0))
+weights_2 = tf.Variable(tf.random_uniform([n_hidden, n_output], -1.0, 1.0))
+
+# Bias
+bias1 = tf.Variable(tf.zeros([n_hidden]))
+bias2 = tf.Variable(tf.zeros([n_output]))
+
+sigmoided_hidden_layer = tf.sigmoid(tf.matmul(inputs, weights_1) + bias1)
+sigmoided_output_layer = tf.sigmoid(tf.matmul(sigmoided_hidden_layer, weights_2) + bias2)
+
+
+#cost = tf.reduce_sum(expected_output - sigmoided_output_layer)
+cost = tf.reduce_mean(- expected_output * tf.log(sigmoided_output_layer) - (1-expected_output) * tf.log(1 - sigmoided_output_layer))
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for step in range(epochs):
+        sess.run(optimizer, feed_dict = {inputs: x_data, expected_output: y_data})
+        print(sess.run(cost, feed_dict = {inputs: x_data, expected_output: y_data}))
+        if step % display_step == 0:
+            #print(sess.run(cost, feed_dict = {inputs: x_data, expected_output: y_data}))
+            continue
+
+    print(sess.run([sigmoided_output_layer], feed_dict = {inputs: x_data, expected_output: y_data}))
+    
