80033a233b296c2340e067da8e66e7971f63c2ec
imran443 <qureshi.443@gmail.com>
2018-04-16 21:18:51 -0400

    Fixing file path of liveCam.py

--- liveCam.py
+++ liveCam.py
@@ -1,17 +1,21 @@
 import urllib.request
 import cv2
+import sys
 import numpy as np
 
 
 def getImage():
     url = 'http://192.168.0.3:8080/shot.jpg'
-    with urllib.request.urlopen(url) as response:
-        imgResp = response.read()
-    
+    try:
+        with urllib.request.urlopen(url) as response:
+            imgResp = response.read()
+    except:
+        print("Please start server up for IP Webcam.")
     imgNp = np.array(bytearray(imgResp), dtype=np.uint8)
     img = cv2.imdecode(imgNp, -1)
-    cv2.imwrite('cap.jpg', img)
-    c = cv2.waitKey(0)
+    print(sys.path[0])
+    cv2.imwrite(sys.path[0] + '\..\images\LiveTest\cap.jpg', img)
+getImage()    
 
 
 
